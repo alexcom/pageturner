186256a58f996957407ddc4ef3f4dc1adf82bd12
alexcom <a.kravet84@gmail.com>
2020-05-18 12:21:59 +0300

    - remove accidentally committed files

--- Уоттс Питер - Водоворот (BlackTracktorist)/page_turner_convert.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-BITRATE=${BITRATE:-128k}
-find . -depth 1 -name "*.mp3" | parallel --lb -k "ffmpeg -y -i {} -map 0:a -c:a aac -b:a $BITRATE {.}.m4a"
--- Уоттс Питер - Водоворот (BlackTracktorist)/page_turner_convert_one.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-BITRATE=${BITRATE:-128k}
-ffmpeg -y \
-  -i 00.mp3 -map 0:a -c:a aac -b:a $BITRATE 00.m4a \
-  </dev/null
